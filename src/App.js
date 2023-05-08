74366c8fb909badebc077f433e1c153dcdb3800d
drlb <dorelb513@gmail.com>
2023-05-08 16:58:46 +0300

    Testing Pull requests

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello
+          Hello1
           <br></br>
           <button>Click here</button>
         </p>
