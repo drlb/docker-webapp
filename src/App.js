48e201974e4fe52833dd0cb6446e2288f3878e81
drlb <dorelb513@gmail.com>
2023-05-08 16:42:54 +0300

    Add new button (click here) to the main welcome page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome!!
+          Hello and welcome to our new site!
           <br></br>
           <button>Click here</button>
         </p>
